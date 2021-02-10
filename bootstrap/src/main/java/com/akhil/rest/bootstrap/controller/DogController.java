c13de0d7447f570b55d15a9d61df3e863a465001
akhilkumarmengani <akhilkumarmengani@gmail.com>
2021-02-11 00:12:49 +0530

    Rest Methods to get Dog Details

--- bootstrap/src/main/java/com/akhil/rest/bootstrap/controller/DogController.java
+++ bootstrap/src/main/java/com/akhil/rest/bootstrap/controller/DogController.java
@@ -1,7 +1,49 @@
 package com.akhil.rest.bootstrap.controller;
 
+import com.akhil.rest.bootstrap.entity.Dog;
+import com.akhil.rest.bootstrap.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DogController {
+
+    private DogService dogService;
+
+    @Autowired
+
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs(){
+        List<Dog> list = dogService.retrieveDogs();
+        return new ResponseEntity<List<Dog>>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/breeds")
+    public ResponseEntity<List<String>> getAllBreeds(){
+        List<String> list = dogService.retrieveDogBreed();
+        return new ResponseEntity<List<String>>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/names")
+    public ResponseEntity<List<String>> getAllNames(){
+        List<String> list = dogService.retrieveDogNames();
+        return new ResponseEntity<List<String>>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/breed/{id}")
+    public ResponseEntity<String> getBreedById(@PathVariable Long id){
+        String breed = dogService.retrieveDogBreedById(id);
+        return new ResponseEntity<String>(breed,HttpStatus.OK);
+    }
+
 }
